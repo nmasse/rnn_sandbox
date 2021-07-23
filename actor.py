3e93bccccc46d136a2c8678d606ac01432aad848
nmasse <nicolas.masse@gmail.com>
2021-07-23 10:36:18 -0500

    Splitting off Actor

---
+++ actor.py
@@ -0,0 +1,113 @@
+import tensorflow as tf
+import numpy as np
+import os
+import layers
+from tensorflow.keras.layers import Dense
+
+
+class Actor:
+    def __init__(self, args, rnn_params, learning_type='supervised'):
+        self._args = args
+        self._rnn_params = rnn_params
+        self.learning_type = learning_type
+        self.create_model()
+        self.opt = tf.keras.optimizers.Adam(args.learning_rate, epsilon=1e-07)
+
+
+    def create_model(self):
+
+        x_input = tf.keras.Input((self._rnn_params.n_bottom_up,)) # Bottom-up input
+        y_input = tf.keras.Input((self._rnn_params.n_top_down,)) # Top-down input
+        h_input = tf.keras.Input((self._rnn_params.n_hidden,)) # Previous activity
+        m_input = tf.keras.Input((self._rnn_params.n_hidden,)) # Previous modulation
+
+        self.RNN = layers.Model(self._rnn_params)
+        h, m = self.RNN.model((x_input, y_input, h_input, m_input))
+        policy = Dense(self._rnn_params.n_actions, activation='linear', name='policy')(h)
+        self.model = tf.keras.models.Model([x_input, y_input, h_input, m_input], [policy, h, m])
+
+
+    def get_action(self, state):
+        # numpy based
+        policy, soma, modulator = self.rnn.model.predict(state)
+        actions = []
+        for i in range(self._args.batch_size):
+            try:
+                action = np.random.choice(self.action_dim, p=policy[i,:])
+            except:
+                action = 0
+            actions.append(action)
+        actions = np.stack(actions, axis = 0)
+
+        return actions, policy, critic, soma, modulator
+
+    def determine_steady_state(self, batch):
+
+        h, m = self.RNN.intial_activity(self._args.batch_size)
+        stimulus = batch[0]
+
+        activity = []
+        mod = []
+
+        for stim in tf.unstack(stimulus,axis=1):
+            bottom_up = 0 * stim[:, :self._rnn_params.n_bottom_up]
+            top_down = stim[:, self._rnn_params.n_bottom_up:]
+            _, h, m = self.model([bottom_up, top_down, h, m], training=True)
+            activity.append(h)
+            mod.append(m)
+
+        activity = tf.stack(activity, axis=1)
+        mod = tf.stack(mod, axis=1)
+
+        mean_activity = tf.reduce_mean(activity[:, -5:, :], axis=1)
+        mean_mod = tf.reduce_mean(mod[:, -5:, :], axis=1)
+
+        return mean_activity, mean_mod, activity
+
+    def run_batch(self, batch, h, m):
+
+        stimulus = batch[0]
+        activity = []
+
+        for stim in tf.unstack(stimulus,axis=1):
+            bottom_up = stim[:, :self._rnn_params.n_bottom_up]
+            top_down = stim[:, self._rnn_params.n_bottom_up:]
+            _, h, m = self.model([bottom_up, top_down, h, m], training=True)
+            activity.append(h)
+
+        return tf.stack(activity, axis=1)
+
+    def reset_optimizer(self):
+
+        for var in self.opt.variables():
+            var.assign(tf.zeros_like(var))
+
+    def train(self, batch, h, m, learning_rate):
+
+        stimulus = batch[0]
+        labels = batch[1]
+        mask = batch[2]
+
+        with tf.GradientTape(persistent=False) as tape: #not sure persistent matters
+
+            policy = []
+            activity = []
+            for stim in tf.unstack(stimulus,axis=1):
+                bottom_up = stim[:, :self._rnn_params.n_bottom_up]
+                top_down = stim[:, self._rnn_params.n_bottom_up:]
+
+                p, h, m = self.model([bottom_up, top_down, h, m], training=True)
+                policy.append(p)
+                activity.append(h)
+            policy = tf.stack(policy, axis=1)
+            activity = tf.stack(activity, axis=1)
+            loss = tf.nn.softmax_cross_entropy_with_logits(labels, policy)
+            loss = tf.reduce_mean(mask * loss)
+
+        grads = tape.gradient(loss, self.model.trainable_variables)
+        grads, _ = tf.clip_by_global_norm(grads, 0.5)
+
+        self.opt.learning_rate.assign(learning_rate)
+        self.opt.apply_gradients(zip(grads, self.model.trainable_variables))
+
+        return loss, activity, policy
--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
-import argparse
 import numpy as np
+import argparse
 import pickle
 import matplotlib.pyplot as plt
 import os
@@ -8,128 +8,19 @@ import copy
 import matplotlib.pyplot as plt
 import layers
 import analysis
-from stimulus import CognitiveTasks
+from actor import Actor
 from TaskManager import TaskManager, default_tasks
 import yaml
-from tensorflow.keras.layers import Dense
 import time
 import uuid
 
-gpu_idx = 1
+gpu_idx = 0
 gpus = tf.config.experimental.list_physical_devices('GPU')
 tf.config.experimental.set_visible_devices(gpus[gpu_idx], 'GPU')
 tf.config.experimental.set_virtual_device_configuration(
     gpus[gpu_idx],
     [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=5000)])
 #os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
-
-
-class Actor:
-    def __init__(self, args, rnn_params, learning_type='supervised'):
-        self._args = args
-        self._rnn_params = rnn_params
-        self.learning_type = learning_type
-        self.create_model()
-        self.opt = tf.keras.optimizers.Adam(args.learning_rate, epsilon=1e-07)
-
-
-    def create_model(self):
-
-        x_input = tf.keras.Input((self._rnn_params.n_bottom_up,)) # Bottom-up input
-        y_input = tf.keras.Input((self._rnn_params.n_top_down,)) # Top-down input
-        h_input = tf.keras.Input((self._rnn_params.n_hidden,)) # Previous activity
-        m_input = tf.keras.Input((self._rnn_params.n_hidden,)) # Previous modulation
-
-        self.RNN = layers.Model(self._rnn_params)
-        h, m = self.RNN.model((x_input, y_input, h_input, m_input))
-        policy = Dense(self._rnn_params.n_actions, activation='linear', name='policy')(h)
-        self.model = tf.keras.models.Model([x_input, y_input, h_input, m_input], [policy, h, m])
-
-
-    def get_action(self, state):
-        # numpy based
-        policy, soma, modulator = self.rnn.model.predict(state)
-        actions = []
-        for i in range(self._args.batch_size):
-            try:
-                action = np.random.choice(self.action_dim, p=policy[i,:])
-            except:
-                action = 0
-            actions.append(action)
-        actions = np.stack(actions, axis = 0)
-
-        return actions, policy, critic, soma, modulator
-
-    def determine_steady_state(self, batch):
-
-        h, m = self.RNN.intial_activity(self._args.batch_size)
-        stimulus = batch[0]
-
-        activity = []
-        mod = []
-
-        for stim in tf.unstack(stimulus,axis=1):
-            bottom_up = 0 * stim[:, :self._rnn_params.n_bottom_up]
-            top_down = stim[:, self._rnn_params.n_bottom_up:]
-            _, h, m = self.model([bottom_up, top_down, h, m], training=True)
-            activity.append(h)
-            mod.append(m)
-
-        activity = tf.stack(activity, axis=1)
-        mod = tf.stack(mod, axis=1)
-
-        mean_activity = tf.reduce_mean(activity[:, -5:, :], axis=1)
-        mean_mod = tf.reduce_mean(mod[:, -5:, :], axis=1)
-
-        return mean_activity, mean_mod, activity
-
-    def run_batch(self, batch, h, m):
-
-        stimulus = batch[0]
-        activity = []
-
-        for stim in tf.unstack(stimulus,axis=1):
-            bottom_up = stim[:, :self._rnn_params.n_bottom_up]
-            top_down = stim[:, self._rnn_params.n_bottom_up:]
-            _, h, m = self.model([bottom_up, top_down, h, m], training=True)
-            activity.append(h)
-
-        return tf.stack(activity, axis=1)
-
-    def reset_optimizer(self):
-
-        for var in self.opt.variables():
-            var.assign(tf.zeros_like(var))
-
-    def train(self, batch, h, m, learning_rate):
-
-        stimulus = batch[0]
-        labels = batch[1]
-        mask = batch[2]
-
-        with tf.GradientTape(persistent=False) as tape: #not sure persistent matters
-
-            policy = []
-            activity = []
-            for stim in tf.unstack(stimulus,axis=1):
-                bottom_up = stim[:, :self._rnn_params.n_bottom_up]
-                top_down = stim[:, self._rnn_params.n_bottom_up:]
-
-                p, h, m = self.model([bottom_up, top_down, h, m], training=True)
-                policy.append(p)
-                activity.append(h)
-            policy = tf.stack(policy, axis=1)
-            activity = tf.stack(activity, axis=1)
-            loss = tf.nn.softmax_cross_entropy_with_logits(labels, policy)
-            loss = tf.reduce_mean(mask * loss)
-
-        grads = tape.gradient(loss, self.model.trainable_variables)
-        grads, _ = tf.clip_by_global_norm(grads, 0.5)
-
-        self.opt.learning_rate.assign(learning_rate)
-        self.opt.apply_gradients(zip(grads, self.model.trainable_variables))
-
-        return loss, activity, policy
 
 
 class Agent:
@@ -232,10 +123,10 @@ class Agent:
                                     np.int32(batch[5]),
                                     list(range(self.n_tasks)))
             print(f'Iteration {j} Loss {loss:1.4f} Accuracy {np.mean(accuracies):1.3f} Mean activity {np.mean(h):2.4f} Time {time.time()-t0:2.2f}')
-            results['loss'].append(loss.numpy())
             results['task_accuracy'].append(accuracies)
+            results['loss'].append(loss.numpy())
             results['final_mean_h'] = np.mean(h.numpy(), axis = (0,2))
-            print(accuracies)
+
 
         pickle.dump(results, open(save_fn, 'wb'))
         self.actor.reset_optimizer()
@@ -283,8 +174,8 @@ parser.add_argument('--n_stim_batches', type=int, default=25)
 parser.add_argument('--learning_rate', type=float, default=0.005)
 parser.add_argument('--n_learning_rate_ramp', type=int, default=10)
 parser.add_argument('--rnn_params_fn', type=str, default='./rnn_params/base_rnn_mod.yaml')
-parser.add_argument('--params_range_fn', type=str, default='./rnn_params/param_ranges.yaml')
-parser.add_argument('--save_path', type=str, default='results3')
+parser.add_argument('--params_range_fn', type=str, default='./rnn_params/param_ranges_no_normalization.yaml')
+parser.add_argument('--save_path', type=str, default='results_no_normalization')
 
 args = parser.parse_args()
 
